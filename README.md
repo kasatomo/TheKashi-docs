1f1ce7a6aebc62ab8c20569c361bdd86beaeb325
kasatomo <kasatomo0721@gmail.com>
2021-08-09 10:02:39 +0900

    init

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# TheKashi-docs
+# The 歌詞職人 docs
+
+https://www.the-kashi.info
---
+++ src/theme.tsx
@@ -0,0 +1,26 @@
+import { createTheme } from '@material-ui/core/styles';
+import red from '@material-ui/core/colors/red';
+
+const theme = createTheme({
+    palette: {
+        primary: {
+            main: '#556cd6',
+        },
+        secondary: {
+            main: '#19857b',
+        },
+        error: {
+            main: red.A400,
+        },
+        background: {
+            default: '#fff',
+        },
+    },
+    typography: {
+        body1: {
+            fontSize: 21,
+        },
+    },
+});
+
+export default theme;
